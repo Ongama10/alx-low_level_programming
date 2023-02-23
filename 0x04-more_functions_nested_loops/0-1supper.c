7c46c15df177b01ffc0a1e7869621a88cf21332d
Ongama10 <ongamantoni67@gmail.com>
2023-02-23 07:35:28 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-1supper.c
+++ 0x04-more_functions_nested_loops/0-1supper.c
@@ -2,19 +2,12 @@
 /**
 * _isupper - checks for uppercase character
 * @c: Variable text
-* Return: Always 0.
+* Return: Always 0
 */
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
